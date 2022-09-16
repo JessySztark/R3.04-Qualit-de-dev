9ff3fff71abb091e6bfc51e73e866d6f3b33262b
Jessy SZTARK <sztarkj@iut-acy.local>
2022-09-16 09:47:19 +0200

    [1.0.2] Fin de TD, Problèmes d'UserControls

--- TD2/TD2/Exo1-View/obj/Debug/net6.0-windows/App.g.i.cs
+++ TD2/TD2/Exo1-View/obj/Debug/net6.0-windows/App.g.i.cs
@@ -59,7 +59,7 @@ namespace Exo1_View {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/Exo1-View;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Exo1-View;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
